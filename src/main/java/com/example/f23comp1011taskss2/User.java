096ea99658a6d5b84e9fcf3b34f336f3d8630ff6
Sehee Hong <seaheehong@gmail.com>
2023-09-19 16:04:29 -0400

    Sep 19 Added validation for the userName

--- src/main/java/com/example/f23comp1011taskss2/User.java
+++ src/main/java/com/example/f23comp1011taskss2/User.java
@@ -16,4 +16,37 @@ public class User {
         setUserName(userName);
         setPhone(phone);
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * This set's the users name, it cannot be blank
+     * @param userName - what ever the user wants it to be
+     */
+    public void setUserName(String userName) {
+        userName = userName.trim(); // remove leading and trailing whitespaces
+        if (userName.matches("[A-z1-9]+"))
+            this.userName = userName;
+        else
+            throw new IllegalArgumentException("username must have at least 1 character and/or number");
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
